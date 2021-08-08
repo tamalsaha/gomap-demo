c782578ca47418a91fa82bbaaad2f793e93cacf6
Tamal Saha <tamal@appscode.com>
2021-08-08 12:35:15 -0700

    added-all

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/tamalsaha/gomap-demo/pkg/api"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"github.com/cespare/xxhash"
 	"github.com/google/go-cmp/cmp"
 	"github.com/kr/pretty"
+	"github.com/tamalsaha/gomap-demo/pkg/api"
+	"gomodules.xyz/sets"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
 func main() {
+	u2 := map[string]sets.Uint64{
+		"a": sets.NewUint64(1, 2),
+	}
+	l := sets.NewUint64(2, 3)
+	fmt.Println(l.Difference(u2["b"]))
+}
+
+func main_() {
 	m := map[api.Matcher][]types.NamespacedName{}
 	fmt.Println("a", m)
 
